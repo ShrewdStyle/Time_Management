84c9d93b986e80fc8f4c201e43a42f197b3a506e
ShrewdStylegit config --global user.name ShrewdStylegit config --global user.name ShrewdStyle <jake.webtech@gmail.com>
2020-04-18 22:55:53 +0100

    Build folder added

---
+++ build/precache-manifest.64545453082260bda6e77abfeda7e6a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09ff2dee3d3bf8764470ef0a0ad2155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5393b9758b9775ec196",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "4300c12c8f5125442f84",
+    "url": "/static/css/main.a6fc354b.chunk.css"
+  },
+  {
+    "revision": "c5393b9758b9775ec196",
+    "url": "/static/js/2.79fb86c8.chunk.js"
+  },
+  {
+    "revision": "a8fa0a685d8af2934ba8ade7768fed07",
+    "url": "/static/js/2.79fb86c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4300c12c8f5125442f84",
+    "url": "/static/js/main.a4341e55.chunk.js"
+  },
+  {
+    "revision": "7e9c3d03a0169434b8cc",
+    "url": "/static/js/runtime-main.cb40dce4.js"
+  },
+  {
+    "revision": "882e370045afa8f16b0b0299bc16dfd4",
+    "url": "/static/media/data.882e3700.jpg"
+  },
+  {
+    "revision": "4c8610413addea0086fe2ef9774408ea",
+    "url": "/static/media/pie.4c861041.png"
+  },
+  {
+    "revision": "2d3f8a61d491e155af3d03e44df52e3f",
+    "url": "/static/media/thankyou.2d3f8a61.jpg"
+  },
+  {
+    "revision": "06428dcd5a306fbc465bcb5bdf54d4e8",
+    "url": "/static/media/timelapse.06428dcd.jpg"
+  }
+]);
